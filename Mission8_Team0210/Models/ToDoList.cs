eab29b3a1f483e87c9120d7557ba2f29cf726498
YuiZoldyck <betasan28@gmail.com>
2023-02-21 09:20:10 -0700

    start buidling model

---
+++ Mission8_Team0210/Models/ToDoList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Mission8_Team0210.Models
+{
+    public class ToDoList
+    {
+        public string Task { get; set; }
+        public DateTime Date { get; set; }
+        public string Quadrant { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}

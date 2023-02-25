7b27d2df0deaed3f6014845d183a91a4f8c0f2c4
YuiZoldyck <betasan28@gmail.com>
2023-02-24 19:02:55 -0700

    controller

--- Mission8_Team0210/Controllers/HomeController.cs
+++ Mission8_Team0210/Controllers/HomeController.cs
@@ -6,32 +6,94 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Mission8_Team0210.Models;
-
 namespace Mission8_Team0210.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
+        private ToDoContex toDoContext { get; set; }
+        public HomeController(ToDoContex x)
+        {
+            toDoContext = x;
+        }
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Quadrants() // action for main quadrants page
         {
             return View();
         }
-
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
         //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         //public IActionResult Error()
         //{
         //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         //}
+        [HttpGet]
+        public IActionResult ToDo() // Loads the page to add tasks
+        {
+            ViewBag.Lists = toDoContext.Lists.ToList();
+            ViewBag.Categories = toDoContext.Categories.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ToDo(ToDoList toDo)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Lists = toDoContext.Lists.ToList();
+                ViewBag.Categories = toDoContext.Categories.ToList();
+                return View("Quadrants");
+            }
+            else
+            {
+                ViewBag.Lists = toDoContext.Lists.ToList();
+                ViewBag.Categories = toDoContext.Categories.ToList();
+                return View();
+            }
+        }
+        // Edit saved tasks
+        public IActionResult Edit(int taskID)
+        {
+            ViewBag.Categories = toDoContext.Categories.ToList();
+            var task = toDoContext.Lists.Single(x => x.TaskId == taskID);
+            return View("ToDo", task);
+        }
+        [HttpPost]
+        public IActionResult Edit(ToDoList list) // checks validity of entered edits and returns to correct page
+        {
+            if (ModelState.IsValid)
+            {
+                toDoContext.Update(list);
+                toDoContext.SaveChanges();
+                return RedirectToAction("Quadrants");
+            }
+            else
+            {
+                ViewBag.Categories = toDoContext.Categories.ToList();
+                return View(list);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int taskID) // specified page for deleting
+        {
+            var task = toDoContext.Lists.Single(x => x.TaskId == taskID);
+            return View(task);
+        }
+        [HttpPost]
+        public IActionResult Delete(ToDoList list) // deletes a specified record in the database
+        {
+            toDoContext.Lists.Remove(list);
+            toDoContext.SaveChanges();
+            return RedirectToAction("Quadrants");
+        }
     }
 }
